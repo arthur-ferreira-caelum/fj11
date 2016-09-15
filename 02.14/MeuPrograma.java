d80b3cde830a44e682b24582d94f883416a5b71a
Arthur Franco Ferreira <arthur.ferreira@caelum.com.br>
2016-09-15 11:35:16 -0300

    Commit do exercicio 2.14 da apostila

---
+++ 02.14/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma {
+    public static void main(String[] args) {
+        //System.out.println("Luke,");
+        //System.out.println("I'm your father!");
+        System.out.println("Luke,\nI'm your father!");
+    } //fim main
+} // fim MeuPrograma
