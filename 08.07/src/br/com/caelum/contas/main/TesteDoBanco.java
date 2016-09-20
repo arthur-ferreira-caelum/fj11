2d4f9354b58ad6128b81bad944f2003bd716f91a
Arthur Franco Ferreira <arthur.ferreira@caelum.com.br>
2016-09-20 12:28:17 -0300

    Adicionando a solução do exercício 08.07 da apostila

---
+++ 08.07/src/br/com/caelum/contas/main/TesteDoBanco.java
@@ -0,0 +1,31 @@
+package br.com.caelum.contas.main;
+
+import br.com.caelum.contas.modelo.Conta;
+
+/**
+ * Programa que faz testes nas classes e c&oatilde;digos para o Banco.
+ * 
+ * @author Arthur Franco Ferreira
+ * @since 20/09/2016
+ */
+public class TesteDoBanco {
+	/**
+	 * M&eacute;todo de execu&ccedil;&atilde;o do programa
+	 * 
+	 * @param args Argumentos passados por par&acirc;metro.
+	 * @since 20/09/2016
+	 */
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setNumero(1);
+		conta.setAgencia("3028");
+		conta.setTitular("Luke");
+		conta.deposita(100);
+
+		System.out.println("Saldo: " + conta.getSaldo());
+
+		conta.saca(10);
+
+		System.out.println("Saldo: " + conta.getSaldo());
+	} // fim main
+} // fim TesteDoBanco
