b306bd2f257a25a4ffc3e288d71d630a89cb81bd
Arthur Franco Ferreira <arthur.ferreira@caelum.com.br>
2016-09-21 09:11:24 -0300

    Correções de erros

--- 07.06/src/br/com/caelum/contas/main/TesteDoBanco.java
+++ 07.06/src/br/com/caelum/contas/main/TesteDoBanco.java
@@ -2,7 +2,20 @@ package br.com.caelum.contas.main;
 
 import br.com.caelum.contas.modelo.Conta;
 
+/**
+ * Programa para testar as classes e opera&ccedil;&otilde;es de banco.
+ * 
+ * @author Arthur Franco Ferreira
+ * @since 18/09/2016
+ *
+ */
 public class TesteDoBanco {
+	
+	/**
+	 * In&iacute;cio do programa de teste.
+	 * 
+	 * @param args Vetor com os argumentos (par&acirc:metros) passado linha de comando ao executar o programa.
+	 */
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 		conta.setNumero(1);
