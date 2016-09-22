918e0a8950253770a1b2320f2412f6eecfdfc3c8
Arthur Franco Ferreira <arthur.ferreira@caelum.com.br>
2016-09-22 09:17:08 -0300

    Adicionando solução do exercicio 10.06 da apostila.

---
+++ 10.06/src/br/com/caelum/contas/main/TestaContas.java
@@ -0,0 +1,25 @@
+package br.com.caelum.contas.main;
+
+import br.com.caelum.javafx.api.main.SistemaBancario;
+
+/**
+ * Programa que testa a interface gr&aacute;fica de contas.
+ * 
+ * @author arthur.ferreira
+ * @since 20/09/2016
+ *
+ */
+public class TestaContas {
+
+	/**
+	 * M&eacute;todo inicial do teste da interface gr&aacute;fica.
+	 * 
+	 * @param args Argumentos do sistema.
+	 * @since 20/09/2016
+	 */
+	public static void main(String[] args) {
+		SistemaBancario.mostraTela(false);
+//		TelaDeContas.main(args);
+	} // fim main
+
+} // fim TestaContas
