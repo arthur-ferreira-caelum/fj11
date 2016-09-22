f4ca5c0e547e5ccfb8342b51443871b1b32160cf
Arthur Franco Ferreira <arthur.ferreira@caelum.com.br>
2016-09-22 09:16:38 -0300

    Adicionando solução do item 09.08 da apostila. Esse não é um exercício, mas é uma discussão interessante e válido ver como fica o código.

---
+++ 09.08/src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -0,0 +1,27 @@
+package br.com.caelum.contas.modelo;
+
+/**
+ * Especifica&ccedil;&atilde;o de uma conta corrente do banco.
+ * 
+ * @author Arthur Franco Ferreira
+ * @since 22/09/2016
+ *
+ */
+public class ContaCorrente extends Conta {
+
+	@Override
+	public void saca(double valor) {
+		//this.saldo -= (valor + 0.10);
+		super.saca(valor + 0.10);
+	}
+	
+	/**
+	 * Define o tipo da conta.
+	 * 
+	 * @return O texto informativo do tipo da conta
+	 */
+	public String getTipo() {
+		return "Conta Corrente";
+	} // fim getTipo
+
+} // fim ContaCorrente
---
+++ 09.08/src/br/com/caelum/contas/modelo/ContaPoupanca.java
@@ -0,0 +1,21 @@
+package br.com.caelum.contas.modelo;
+
+/**
+ * Especifica&ccedil;&atilde;o de uma conta poupan&ccedil;a do banco.
+ * 
+ * @author Arthur Franco Ferreira
+ * @since 22/09/2016
+ *
+ */
+public class ContaPoupanca extends Conta {
+
+	/**
+	 * Define o tipo da conta.
+	 * 
+	 * @return O texto informativo do tipo da conta
+	 */
+	public String getTipo() {
+		return "Conta Poupança";
+	} // fim getTipo
+	
+} // fim ContaPoupanca
