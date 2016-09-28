dfca3b69bf11ab52fa402f7629f578d5d22553e5
Arthur Franco Ferreira <arthur.ferreira@caelum.com.br>
2016-09-28 09:13:38 -0300

    Adicionando solução do exercício 14.05 da apostila

---
+++ 14.05/src/br/com/caelum/contas/modelo/Conta.java
@@ -0,0 +1,142 @@
+package br.com.caelum.contas.modelo;
+
+import br.com.caelum.contas.exceptions.SaldoInsuficienteException;
+
+/**
+ * Especifica&ccedil;&atilde;o de uma conta de banco.
+ * 
+ * @author Arthur Franco Ferreira
+ * @since 20/09/2016
+ *
+ */
+public abstract class Conta {
+
+	// Atributos de instancia
+
+	private double saldo;
+	private String titular;
+	private int numero;
+	private String agencia;
+
+	// Metodos
+
+	/**
+	 * Adiciona um valor ao saldo da conta.
+	 * 
+	 * @param valor
+	 *            O valor a ser adicionado.
+	 * @since 20/09/2016
+	 */
+	public void deposita(double valor) {
+		// Valida se o valor passado eh negativo
+		// Caso seja, lança um erro. Nada abaixo da da exception eh executado
+		// por conta do erro.
+		if (valor < 0)
+			throw new IllegalArgumentException("Você tentou depositar um valor negativo");
+
+		// Essa linha so sera executada se o valor for positivo
+		this.saldo += valor;
+	} // fim deposita
+
+	/**
+	 * Realiza uma retirada da conta.
+	 * 
+	 * @param valor
+	 *            O valor a ser retirado.
+	 * @since 20/09/2016
+	 */
+	public void saca(double valor) {
+		// Valida se o valor passado eh negativo
+		// Caso seja, lança um erro. Nada abaixo da da exception eh executado
+		// por conta do erro.
+		if (valor < 0)
+			throw new IllegalArgumentException("Você tentou depositar um valor negativo");
+
+		// Valida se o valor passado eh menor do que o saldo
+		if (valor > this.saldo)
+			throw new SaldoInsuficienteException(valor);
+
+		// Essa linha so sera executada se o valor for positivo
+		this.saldo -= valor;
+	} // fim saca
+
+	/**
+	 * Realiza uma transfer&ecirc;ncia de valor entre contas.
+	 * 
+	 * @param valor
+	 *            O valor a ser transferido.
+	 * @param destino
+	 *            A conta de destino a ser creditada.
+	 */
+	public void transfere(double valor, Conta destino) {
+		this.saca(valor);
+		destino.deposita(valor);
+	} // fim transfere
+
+	/**
+	 * Define o tipo da conta.
+	 * 
+	 * @return O texto informativo do tipo da conta.
+	 */
+	public abstract String getTipo();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "[titular=" + this.titular + ", numero=" + this.numero + ", agencia=" + this.agencia + ", saldo="
+				+ this.saldo + "]";
+	} // fim toString
+
+	@Override
+	public boolean equals(Object obj) {
+		// Se a referencia passada for null, nao eh igual
+		if (obj == null)
+			return false;
+
+		// Se a referencia passada nao for uma instancia de Conta pelo menos,
+		// nao eh igual
+		if (!(obj instanceof Conta))
+			return false;
+
+		// Agora que garantimos que a referencia passada eh de Conta, podemos
+		// fazer o casting.
+		Conta outraConta = (Conta) obj;
+
+		return (this.numero == outraConta.getNumero()) && (this.agencia.equals(outraConta.getAgencia()));
+	}
+
+	// Getters e Setters
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+} // fim Conta
